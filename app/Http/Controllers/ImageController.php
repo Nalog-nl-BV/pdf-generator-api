3be66a00a6de86c506ddced028eebce6064ffa6d
Oleg <oleg.bortovsky@gmail.com>
2023-05-15 13:16:07 +0300

    Add test code to ImageController.php for checking if all packs work correctly.

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -10,34 +10,29 @@ class ImageController extends Controller
 {
     public function convertPdfToImage()
     {
-        if (extension_loaded('imagick')) {
-            return "Imagick extension is installed.";
-        } else {
-            return "Imagick extension is not installed.";
+        $pdfPath = storage_path('app/public/Certificates/test2.pdf');
+        $outputPath = storage_path('app/public/Certificates/test2.jpg');
+
+        // Convert the first page of the PDF to an image
+        try {
+            $pdf = new Pdf($pdfPath);
+            $pdf->saveImage($outputPath);
+            return [
+                "code" => 200,
+                "status" => "success",
+                "message" => null,
+                "data" => $outputPath
+            ];
+        } catch (PdfDoesNotExist $e) {
+            return [
+                "code" => 400,
+                "status" => "error",
+                "message" => $e->getMessage(),
+                "data" => null
+            ];
         }
-//        $pdfPath = storage_path('app/public/Certificates/test2.pdf');
-//        $outputPath = storage_path('app/public/Certificates/test2.jpg');
-//
-//        // Convert the first page of the PDF to an image
-//        try {
-//            $pdf = new Pdf($pdfPath);
-//            $pdf->saveImage($outputPath);
-//            return [
-//                "code" => 200,
-//                "status" => "success",
-//                "message" => null,
-//                "data" => $outputPath
-//            ];
-//        } catch (PdfDoesNotExist $e) {
-//            return [
-//                "code" => 400,
-//                "status" => "error",
-//                "message" => $e->getMessage(),
-//                "data" => null
-//            ];
-//        }
-//        // Additional options:
-//        // ->setResolution(300) // Set the image resolution (default is 150)
-//        // ->setOutputFormat('png') // Set the output image format (default is jpg)
+        // Additional options:
+        // ->setResolution(300) // Set the image resolution (default is 150)
+        // ->setOutputFormat('png') // Set the output image format (default is jpg)
     }
 }
