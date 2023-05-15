5bf3a812ac63ab45befafd0298d0abfa53bf8dc1
Oleg <oleg.bortovsky@gmail.com>
2023-05-15 13:06:11 +0300

    Add ImageController for convert PDF to Image (Test version)

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\PdfToImage\Exceptions\PdfDoesNotExist;
+use Spatie\PdfToImage\Pdf;
+
+class ImageController extends Controller
+{
+    public function convertPdfToImage()
+    {
+        $pdfPath = storage_path('app/public/Certificates/test2.pdf');
+        $outputPath = storage_path('app/public/Certificates/test2.jpg');
+
+        // Convert the first page of the PDF to an image
+        try {
+            $pdf = new Pdf($pdfPath);
+            $pdf->saveImage($outputPath);
+            return [
+                "code" => 200,
+                "status" => "success",
+                "message" => null,
+                "data" => $outputPath
+            ];
+        } catch (PdfDoesNotExist $e) {
+            return [
+                "code" => 400,
+                "status" => "error",
+                "message" => $e->getMessage(),
+                "data" => null
+            ];
+        }
+        // Additional options:
+        // ->setResolution(300) // Set the image resolution (default is 150)
+        // ->setOutputFormat('png') // Set the output image format (default is jpg)
+    }
+}
