fb9c98811f89424fa6c2749726cffacd8b1da687
Oleg <oleg.bortovsky@gmail.com>
2023-05-16 14:02:04 +0300

    Add new template for swift-bank-tax-office controller.

---
+++ resources/views/swift-bank-tax-office.blade.php
@@ -0,0 +1,159 @@
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;900&display=swap');
+
+    .container {
+        width: 752px;
+        height: 1000px;
+        margin: 0 auto;
+
+        padding: 10px 40px;
+        position: relative;
+    }
+
+    .logo {
+        margin: 20px;
+    }
+
+    .confidential {
+        margin-top: 30px;
+    }
+
+    .address {
+        margin-top: 20px;
+    }
+
+    .address p {
+        margin: 5px 0;
+    }
+
+    .date {
+        position: absolute;
+        right: 40px;
+        top: 245px
+    }
+
+    .date p {
+        margin: 5px 0;
+    }
+
+    .main-content {
+        margin-top: 30px;
+    }
+
+    .signature {
+        margin-top: 20px;
+        margin-bottom: 10px;
+        height: 80px;
+    }
+
+    .contact_left p,
+    .contact_right p {
+        margin: 4px 0;
+    }
+
+    .contact_left {
+        position: absolute;
+        left: 60px;
+    }
+
+    .contact_right {
+        position: absolute;
+        right: 130px;
+    }
+
+    .footer {
+        margin-top: 113px;
+        text-align: center;
+    }
+</style>
+<body class="container">
+<div class="logo">
+    <img src="https://internal.nalog.nl/wp-content/uploads/2022/12/Skype_Picture_2022_12_24T16_17_37_254Z.png"
+         alt="logo" width="200">
+</div>
+
+<h4 class="confidential">
+    VERTROUWELIJK
+</h4>
+<div class="address">
+    <p>Belastingdienst</p>
+    <p>Antwoordummer 21020</p>
+    <p>6400XC Heerlen</p>
+</div>
+
+<div class="date">
+    <p style="padding-right: 20px;">
+        Datum
+    </p>
+    <p>
+        5/16/2023
+    </p>
+</div>
+
+<div class="main-content">
+    <p>Onze referentle: 543/2022</p>
+
+    <p>Tem name: <span>A.P. Stamere </span>BSN: <span>422698568 </span>GD: <span>15.11.1998</span></p>
+
+    <p style="font-weight: bold;">Betreft: buitealandse rekenlugnummer doorgeven</p>
+
+    <p>Geachte heer/mevrouw,</p>
+
+    <p>Hierbij aburen wij de gegevena van bankrekenlngnummer voor alle belastingen van onze klant A.P.Stamere.</p>
+
+    <p>
+        Naam van bank: Swedbank <br>
+        Rekeninghouder: ANNA PEULA STAMERE <br>
+        IBAN: LV55 HABA 0551 0473 8575 8 <br>
+        Adres van bank: RAINA 44-43, RIGA, LV-1063, Latvia
+    </p>
+
+    <p>Bijlagen:</p>
+
+    <p style="padding-left: 30px;">
+        1. kopie van paspot <br>
+        2. kopie van bankschrift
+    </p>
+
+    <p>
+        Hopalijk habben wij uw voldoende geinformeerd. Indien u meer informatie wenst, kunt i met ons contact opnemen op
+        telefoonnummer 085-5400200 of via email: info@nalog.nl van maandag t/m vrijdag van 10-00 bot 18-00.
+    </p>
+
+    <p>
+        Met vriendelijke groet, <br>
+        Administratief medewerker
+    </p>
+</div>
+
+<div class="signature">
+    <div class="name">
+        Julia Jurik
+    </div>
+    <div class="stamp">
+
+    </div>
+</div>
+
+<div class="contact_left">
+    <div>
+        <p>Nalog.nl B.V.</p>
+        <p>KVK: 66449898</p>
+        <p>Beconnummer: 596358</p>
+        <p>BTW-nummer: NL856558692B01</p>
+    </div>
+</div>
+
+<div class="contact_right">
+    <div>
+        <p>Dukatenburg 82 3437AE Nieuwegein</p>
+        <p>+31 (0) 85 5400 200</p>
+        <p>+31 (0) 65 0128 355</p>
+        <p>info@nalog.nl</p>
+    </div>
+</div>
+
+<div class="footer">
+    Referentienunumen: 0cbdfkmk03j358gnlfgn3ng0n23
+</div>
+</body>
