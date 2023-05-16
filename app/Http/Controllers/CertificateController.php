6de8fa8886e43820755da57e02dec62969ec248c
Oleg <oleg.bortovsky@gmail.com>
2023-05-16 11:30:45 +0300

    Change fonts and size of certificate.

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -6,10 +6,10 @@ use App\Http\Requests\CertificateGenerateRequest;
 use Exception;
 use Illuminate\Http\Client\Response;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
-use Mpdf\HTMLParserMode;
 use Mpdf\Output\Destination;
 
 class CertificateController extends Controller
@@ -18,13 +18,13 @@ class CertificateController extends Controller
      * @param CertificateGenerateRequest $request
      * @return JsonResponse|Response
      */
-    public function CertificateGenerate(CertificateGenerateRequest $request)
+    public function CertificateGenerate(Request $request)
     {
         try {
-            $data = $request->validated();
+            $data = $request;
 
-            $widthInPx = 1280;
-            $heightInPx = 905;
+            $widthInPx = 1533;
+            $heightInPx = 700;
             $widthInMm = $widthInPx * 0.2645833333;
             $heightInMm = $heightInPx * 0.2645833333;
 
@@ -42,31 +42,26 @@ class CertificateController extends Controller
                     base_path("public/fonts"),
                 ]),
                 'fontdata' => $fontData + [ // lowercase letters only in font key
-                        "caveat" => [
-                            'R' => "Caveat/static/Caveat-Bold.ttf",
-                            'useOTL' => 0xFF,
-                            'useKashida' => 75,
-                        ],
                         "montserrat" => [
                             'R' => "Montserrat/static/Montserrat-Regular.ttf",
-                            'M' => "Montserrat/static/Montserrat-Medium.ttf",
                             'B' => "Montserrat/static/Montserrat-Bold.ttf",
                             'useOTL' => 0xFF,
                             'useKashida' => 75,
                         ],
-                        "pacifico" => [
-                            'R' => "Pacifico/Pacifico-Regular.ttf",
+                        "montserrat-black" => [
+                            'R' => "Montserrat/static/Montserrat-SemiBold.ttf",
+                            'B' => "Montserrat/static/Montserrat-Black.ttf",
                             'useOTL' => 0xFF,
                             'useKashida' => 75,
                         ],
                     ],
-                'default_font' => 'caveat'
+                'default_font' => 'montserrat'
             ]);
 
             $documentFileName = $data["fileName"] . ".pdf";
             $imageName = $data["fileName"] . ".jpg";
 
-            $document->SetDefaultBodyCSS('background', "url('https://internal.nalog.nl/wp-content/uploads/2023/05/photo_2023-05-08_14-55-30.jpg') no-repeat left center");
+            $document->SetDefaultBodyCSS('background', "url('https://internal.nalog.nl/wp-content/uploads/2023/05/reclAsset-1-4.png') no-repeat left center");
 
             $html = new HtmlController();
 
@@ -81,6 +76,19 @@ class CertificateController extends Controller
                 "offers" => $data["offers"],
                 "image" => $data["image"],
             ];
+
+//            $data = [
+//                "number" => "12WEd1",
+//                "date" => "09/05/2024",
+//                "clientName" => "Oleg Bortovsky",
+//                "employeeName" => "Oleg Bortovsky",
+//                "discount" => "Є172",
+//                "discountType" => "type",
+//                "offer" => [
+//                    "title" => "открытие BV",
+//                    "link" => "https://www.nalog.nl/vse-uslugi/yuridiceskim-licam/registraciya-firm-2/registraciya-bv/"
+//                ]
+//            ];
 
             $document->WriteHTML($html->getHtml("certificate-eng", $data));
 
