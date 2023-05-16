5e735652bdaaa227369242439b4d112a1e3ac0b8
Oleg <oleg.bortovsky@gmail.com>
2023-05-16 14:00:51 +0300

    Add new Controller for endpoint swift-bank-tax-office. And add request.

---
+++ app/Http/Controllers/SwiftBankTaxController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PDFDeleteRequest;
+use App\Http\Requests\PDFGenerateRequest;
+use App\Http\Requests\SwiftBankTaxRequest;
+use Exception;
+use Illuminate\Http\Client\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
+use Mpdf\HTMLParserMode;
+use Mpdf\Mpdf;
+use Mpdf\Output\Destination;
+
+class SwiftBankTaxController extends Controller
+{
+    public function draw() {
+        return view("swift-bank-tax-office");
+    }
+
+    public function index(SwiftBankTaxRequest $request)
+    {
+        try {
+            $data = $request->validated();
+
+            $document = new Mpdf();
+
+            $documentFileName = $data["fileName"] . ".pdf";
+
+            $html = new HtmlController();
+
+            $data = [];
+
+            $document->WriteHTML($html->getHtml("swift-bank-tax-office", $data));
+
+            Storage::disk('public')->put('/PDFs/' . $documentFileName, $document->Output($documentFileName, Destination::STRING_RETURN));
+
+            return Http::post('https://hub.nalog.nl/api/v1/storage/store', [
+                "token" => $data["token"],
+                "bucket_name" => "nalog",
+                "dir_name" => "test",
+                "is_public" => true,
+                "files_data" => [
+                    Storage::disk('public')->url('/PDFs/' . $documentFileName) => Hash::make(date('Y-m-d H:i:s'))
+                ]
+            ]);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'code' => 500,
+                'status' => "error",
+                'message' => "Server error",
+                'data' => $e->getMessage(),
+            ]);
+        }
+    }
+}
---
+++ app/Http/Requests/SwiftBankTaxRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\Rule;
+
+class SwiftBankTaxRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "fileName" => "required|string",
+            "token" => [
+                'required',
+                Rule::in([env('PDF_TOKEN')]),
+            ],
+        ];
+    }
+
+    /**
+     * @param Validator $validator
+     * @return void
+     */
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'code'   => 400,
+            'status'   => "error",
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+}
